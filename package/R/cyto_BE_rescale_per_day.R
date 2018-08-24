69d7189493e532863e07748c28fe86006d2b3721
Helena Todorov <helena_t@hotmail.fr>
2018-08-24 10:20:56 +0200

    New function to rescale data to fix batch effect on functional marker MFIs

---
+++ package/R/cyto_BE_rescale_per_day.R
@@ -0,0 +1,22 @@
+BE_rescale_per_day <- function(aggreg_table){
+  days <- names(table(aggreg_table$day_id))
+  min_day1 <- apply(aggreg_table[which(aggreg_table$day_id==days[1]),1:10],2,
+                    function(x) quantile(x, 0.001))
+  max_day1 <- apply(aggreg_table[which(aggreg_table$day_id==days[1]),1:10],2,
+                    function(x) quantile(x, 0.999))
+  aggreg_table <- as.data.frame(aggreg_table)
+
+  for(i in 2:length(days)){
+    for (marker in colnames(aggreg_table)[1:10]){
+      # aggreg_table[which(aggreg_table$day_id==days[[i]]), ..marker] <- (((aggreg_table[which(aggreg_table$day_id==days[[i]]), ..marker] -
+      #                                                                     min(aggreg_table[which(aggreg_table$day_id==days[[i]]), ..marker])) /
+      #                                                                    (max(aggreg_table[which(aggreg_table$day_id==days[[i]]), ..marker]) - min(aggreg_table[which(aggreg_table$day_id==days[[i]]), ..marker]))) *
+      #                                                                   (max_day1[marker] - min_day1[marker]) ) + min_day1[marker]
+
+      aggreg_table[which(aggreg_table$day_id==days[[i]]), marker] <- scales::rescale(aggreg_table[which(aggreg_table$day_id==days[[i]]), marker],
+                                                                                       to = c(min_day1[marker], max_day1[marker]))
+    }
+  }
+  aggreg_table
+}
+
