08aed4063608960d04dc0a4d7ba4643fd0a262a6
Helena Todorov <helena_t@hotmail.fr>
2018-09-03 12:08:02 +0200

    Doesn't work if flowCore is not loaded

--- package/R/cyto_fcs_to_agg.R
+++ package/R/cyto_fcs_to_agg.R
@@ -13,10 +13,10 @@
 fcs_to_agg <- function(fcs_dir, fcs_names, seed, cTotal, output_name){
   setwd(dir = fcs_dir)
   set.seed(seed)
-  ff_agg <- AggregateFlowFrames(fcs_names,
+  ff_agg <- FlowSOM::AggregateFlowFrames(fcs_names,
                                 cTotal= cTotal, writeOutput = TRUE,
                                 outputFile = output_name)
-  ff_agg <- transform(ff_agg,
-                      transformList(colnames(ff_agg)[c(3,17,28:62,71)], arcsinhTransform(b=1/5, a=0, c=0)))
+  ff_agg <- flowCore::transform(ff_agg,
+                      flowCore::transformList(colnames(ff_agg@exprs)[c(3,17,28:62,71)], arcsinhTransform(b=1/5, a=0, c=0)))
   return(ff_agg)
 }
