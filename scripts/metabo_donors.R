18b7de47b23e85b8ff97aa8e28aa01e49afcdbd9
Helena Todorov <helena_t@hotmail.fr>
2018-09-07 13:54:59 +0200

    Updated script, now also generating a table containing subpathways per patient instead of metabolites (subpathway value = mean of all metabolites from this subpathway)

--- scripts/metabo_donors.R
+++ scripts/metabo_donors.R
@@ -8,28 +8,28 @@ library(adegraphics)
 ########  PREPROCESSING  ########
 #################################
 
-sample_subset <- read.xlsx("~/Documents/VIB/Projects/Integrative_Paris/documents_22:02:18/CYTOF_David_Michonneau/Data synthesis local cohort Saint-Louis 032018.xlsx", 
-                     check.names = FALSE) %>% 
+sample_subset <- read.xlsx("~/Documents/VIB/Projects/Integrative_Paris/documents_22:02:18/CYTOF_David_Michonneau/Data synthesis local cohort Saint-Louis 032018.xlsx",
+                     check.names = FALSE) %>%
   mutate(DATEOFCYTOFEXPERIMENT = as.Date(DATEOFCYTOFEXPERIMENT, "%d.%m.%Y"),
-         GROUP = tolower(GROUP)) %>% 
-  dplyr::filter(!is.na(METABONAME)) %>% 
-  dplyr::filter(grepl("D",Id.Cryostem.R)) %>% 
-  dplyr::select_if(~sum(!is.na(.)) > 0) 
+         GROUP = tolower(GROUP)) %>%
+  dplyr::filter(!is.na(METABONAME)) %>%
+  dplyr::filter(grepl("D",Id.Cryostem.R)) %>%
+  dplyr::select_if(~sum(!is.na(.)) > 0)
 rownames(sample_subset) <- sample_subset$METABONAME
-  
-save(sample_subset, file="~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/sample_donors.RData")
 
-merged <- merge_metabo_metadata(metadata_file = "~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/sample_donors.RData",
-                                metabo_file = "~/Documents/VIB/Projects/Integrative_Paris/documents_22:02:18/CYTOF_David_Michonneau/Metabolomic local cohort Saint-Louis_filtered.xlsx")
-recip_meta <- merged$subset_meta
-meta_metabo <- merged$meta_metabo
-data_metabolites <- merged$data_metabolites
+save(sample_subset, file="~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/donors/sample_donors.RData")
+
+info <- extract_info_metabo(metadata_file = sample_subset,
+                            metabo_file = "~/Documents/VIB/Projects/Integrative_Paris/documents_22:02:18/CYTOF_David_Michonneau/Metabolomic local cohort Saint-Louis_filtered.xlsx")
+donor_meta <- info$subset_meta
+meta_metabo <- info$meta_metabo
+data_metabolites <- info$data_metabolites
 
 ##### filtering #####
 
 data_metabolites <- data_metabolites[,-which((meta_metabo[2,]=="Xenobiotics")&(meta_metabo[1,]=="Drug"))] # rm drug xenobiotics
 
-colsums <- by(data_metabolites, recip_meta$GROUP, # identify metabolites <50% in each group
+colsums <- by(data_metabolites, sample_subset$GROUP, # identify metabolites <50% in each group
               FUN = function(x) {colSums(is.na(x)) >= nrow(x)*0.5})
 high_na <- which(colsums[[1]]&colsums[[2]]&colsums[[3]]) # identify metabolites <50% in all groups
 data_metabo <- data_metabolites[ , -high_na]
@@ -53,14 +53,15 @@ rownames(data_metabo) <- rnames
 mat2use <- merge.data.frame(as.data.frame(sample_subset[,2], row.names = rownames(sample_subset)),
                             data_metabo, by = "row.names") %>%
   tibble::column_to_rownames(var="Row.names")
-  
+
 logdata <- LogTransform(mat2use)
+save(logdata, file = "logdata_metabo_donors.RData")
 normdata <- Normalise(logdata$output, method = "median")
 colnames(normdata$output) <- colnames(logdata$output)
 norm_data <- normdata$output
 
 # merge dataframes:
-big_mat <- merge.data.frame(normdata$output, recip_meta, by = "row.names") %>% 
+big_mat <- merge.data.frame(normdata$output, recip_meta, by = "row.names") %>%
   tibble::column_to_rownames("Row.names")
 
 save(norm_data, file="norm_data.RData")
@@ -129,6 +130,61 @@ MetBoxPlots(mat2use, "ursodeoxycholate",cols = c("blue","green"),main = "ursodeo
 
 TwoGroupPlots(mat2use[,-1], res$output[,1], foldchanges = res$output[,4], pvalues = res$output[,2],
               padjmethod = "BH", fcutoff = log(2), pcutoff = 0.05)
+
+
+
+####################################################
+####### Turn metabolites -> subpathway table #######
+####################################################
+
+load("outputs/data/metabo/donors/logdata_metabo_donors.RData")
+load("outputs/data/metabo/donors/meta_metabo.RData")
+load("outputs/data/metabo/donors/sample_donors.RData")
+
+data_metabo <- as.data.frame(logdata$output[,-1])
+meta_metabo <- meta_metabo[,which(as.character(meta_metabo[3,])%in%colnames(data_metabo))]
+data_metabo <- data_metabo[,which(colnames(data_metabo)%in%as.character(meta_metabo[3,]))]
+
+library(data.table)
+t_data_metabo <- t(data_metabo)
+subpath_info <- as.character(meta_metabo[1,])
+data_meta <- as.data.table(t_data_metabo)
+binded <- cbind(data_meta, subpath_info)
+
+subpaths <- binded[,lapply(.SD, median), by=subpath_info]
+subpaths <- as.data.frame(subpaths) %>% column_to_rownames("subpath_info")
+subpaths <- t(subpaths)
+save(subpaths, file = "subpaths_table_donors.RData")
+
+### Normalise :
+subpaths_norm <- apply(subpaths, 2, scale)
+rownames(subpaths_norm) <- rownames(subpaths)
+superpaths <- lapply(seq_along(colnames(subpaths_norm)), function(i){
+  superpath <- as.character(meta_metabo[2,which(meta_metabo[1,]==colnames(subpaths_norm)[i])])[1]
+})
+superpaths <- as.data.frame(unlist(superpaths))
+rownames(superpaths) <- colnames(subpaths_norm)
+
+# heatmap of subpaths, annotated by group, gender, and superpaths
+pheatmap::pheatmap(subpaths_norm,
+                   annotation_row = sample_subset[,c("GROUP", "GENDER")],
+                   annotation_colors = list("GROUP"=c("non_tolerant"="#e31a1c90",
+                                                      "primary_tolerant"="#00FF0090",
+                                                      "secondary_tolerant"="#0000FF90")),
+                   annotation_col = superpaths)
+
+## random forest:
+subpaths_annot <- cbind.data.frame(sample_subset[,c("GROUP", "GENDER")], subpaths, by = "row_names")
+subpaths_annot <- subpaths_annot[,-c(2,80)]
+colnames(subpaths_annot) <- make.names(colnames(subpaths_annot))
+subpaths_annot$GROUP <- as.factor(subpaths_annot$GROUP)
+rf_metabo <- randomForest::randomForest(GROUP~., subpaths_annot)
+rf_metabo
+
+subpaths_tol <- subpaths_annot[which(subpaths_annot$GROUP!="non_tolerant"),]
+subpaths_tol$GROUP <- as.factor(as.character(subpaths_tol$GROUP))
+rf_metabo <- randomForest::randomForest(GROUP~., subpaths_tol)
+rf_metabo
 
 
 
