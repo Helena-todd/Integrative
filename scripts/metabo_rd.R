43bd67dd49bc700db2c8eca2aab414f45b296b9e
Helena Todorov <helena_t@hotmail.fr>
2019-03-12 16:17:37 +0100

    combined all metabo scripts into one

--- scripts/metabo_rd.R
+++ scripts/metabo_rd.R
@@ -4,16 +4,17 @@ library(metabolomics)
 library(BioGVHD)
 library(tidyverse)
 library(data.table)
+library(dendextend)
 
 
 #################################
 ########  PREPROCESSING  ########
 #################################
 
-samp_rd <- read.xlsx("~/Documents/VIB/Projects/Integrative_Paris/documents_RNAseq_14:01:19/Data synthesis local cohort Saint-Louis 17012019.xlsx")
+samp_rd <- read.xlsx("~/Documents/VIB/Projects/Integrative_Paris/Local_cohort/Data synthesis local cohort Saint-Louis 17012019.xlsx")
 
 info <- extract_info_metabo(metadata_file = samp_rd,
-                                metabo_file = "~/Documents/VIB/Projects/Integrative_Paris/documents_22:02:18/CYTOF_David_Michonneau/Metabolomic local cohort Saint-Louis_filtered.xlsx")
+                            metabo_file = "~/Documents/VIB/Projects/Integrative_Paris/Local_cohort/Metabo/Metabolomic local cohort Saint-Louis_filtered.xlsx")
 rd_meta <- info$subset_meta
 meta_metabo <- info$meta_metabo
 data_metabolites <- info$data_metabolites
@@ -21,6 +22,57 @@ data_metabolites <- info$data_metabolites
 ##### filtering #####
 
 data_metabolites <- data_metabolites[,-which((meta_metabo[2,]=="Xenobiotics")&(meta_metabo[1,]=="Drug"))] # rm drug xenobiotics
+data_metabo_national <- read.xlsx("~/Documents/VIB/Projects/Integrative_Paris/National_cohort/Metabo/Metabo NATIONAL cohort CRYOSTEM.xlsx")
+national_metabolites <- data_metabo_national$X1[-c(1,2)]
+# remove metabolites that are not present in both the national and the St_Louis cohort :
+data_metabolites <- data_metabolites[,which(colnames(data_metabolites) %in% national_metabolites)]
+
+##### generate figure similar to David's #####
+names_patients <- rownames(data_metabolites)
+
+big_mat <- data_metabolites %>%
+  mutate_all(as.numeric) %>%
+  mutate(METABONAME = names_patients) %>%
+  left_join(rd_meta[,c("GROUP", "COUPLENUMBER", "METABONAME")], by = "METABONAME") %>%
+  mutate(status = ifelse(test = grepl("D", METABONAME), yes = "D", no = "R"))
+
+na_pctgs <- big_mat %>%
+  group_by(GROUP, status) %>%
+  summarize_all(funs(sum(is.na(.)) / length(.)))
+
+melted <- melt(na_pctgs, id = 1:2)
+
+## marche pas:
+# to_rm <- melted %>%
+#   group_by(variable) %>%
+#   summarize_if(is.integer, funs(all(. > 0.5)))
+
+to_rm <- lapply(names(table(melted$variable)), function(metabolite){
+  values <- melted$value[which(melted$variable == metabolite)]
+  all(values > 0.5)
+})
+
+names2rm <- as.character(names(table(melted$variable))[which(to_rm == T)])
+plot2rm <- melted[which(melted$variable %in% names2rm),]
+table_pctg_na <- plot2rm %>% arrange(status) %>%
+  mutate(variable = paste0(variable, GROUP))
+
+library(ggplot2)
+ggplot(data = table_pctg_na,
+       mapping = aes(x = variable, fill = GROUP,
+                     y = ifelse(test = status == "D",
+                                yes = -value, no = value))) +
+  geom_bar(stat = "identity") +
+  scale_y_continuous(labels = abs, limits = max(table_pctg_na$value) * c(-1,1)) +
+  labs(y = "pctg of NA") +
+  coord_flip()
+
+##### new filtering : > 50% na in all groups, donors and recipients
+
+high_na <- which(to_rm==T)
+data_metabo <- data_metabolites[ , -high_na]
+
+##### previous filtering : >50% in at least one group
 
 colsums <- by(data_metabolites, rd_meta$GROUP, # identify metabolites <50% in each group
               FUN = function(x) {colSums(is.na(x)) >= nrow(x)*0.5})
@@ -66,9 +118,9 @@ save(rd_meta, file="~/Documents/VIB/Projects/Integrative_Paris/Integrative/outpu
 
 
 
-################################
-##########  ANALYSIS  ##########
-################################
+#################################################
+##########  ANALYSIS  recipients only  ##########
+#################################################
 
 load("outputs/data/metabo/recip/norm_data.RData")
 load("outputs/data/metabo/recip/meta_metabo.RData")
@@ -238,29 +290,65 @@ load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo
 load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/meta_metabo.RData")
 load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/big_mat.RData")
 load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/rd_meta.RData")
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/norm_data.RData")
 
-### Look at the subpathways instead of metabolites
+### Look at clusters of similar metabolites instead of metabolites directly
 data_metabo <- as.data.frame(logdata[,-1])
 meta_metabo <- meta_metabo[,which(as.character(meta_metabo[3,])%in%colnames(data_metabo))]
-data_metabo <- data_metabo[,which(colnames(data_metabo)%in%as.character(meta_metabo[3,]))]
 
-t_data_metabo <- t(data_metabo)
-subpath_info <- as.character(meta_metabo[1,])
-data_meta <- as.data.table(t_data_metabo)
-binded <- cbind(data_meta, subpath_info)
+subst_mat <- big_mat %>%
+  group_by(COUPLENUMBER) %>%
+  summarise_if(is.numeric,~.[1]-.[2])
 
-subpaths <- binded[,lapply(.SD, mean), by=subpath_info]
-subpaths <- as.data.frame(subpaths) %>% column_to_rownames("subpath_info")
-subpaths <- as.data.frame(t(subpaths))
+data_metabo <- subst_mat[,-c(1,542:557)]
+rownames(data_metabo) <- subst_mat$COUPLENUMBER
+info_gr_couple <- unique(big_mat[,c("Group", "COUPLENUMBER")]) %>%
+  rownames_to_column("METABONAME") %>%
+  column_to_rownames("COUPLENUMBER")
+gr_info <- info_gr_couple[rownames(data_metabo),"Group"]
 
-# I add the group and couple nb info in 2 extra columns:
-sub_data <- cbind(logdata[,1], subpaths)
-colnames(sub_data)[1] <- "group"
-sub_data <- cbind(big_mat$COUPLENUMBER, sub_data)
-colnames(sub_data)[1] <- "couplenb"
 
-save(sub_data, file = "~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/sub_data.RData")
-load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/sub_data.RData")
+# kmeans clustering to find groups of metabolites
+km100 <- kmeans(t(data_metabo), centers = 100, iter.max = 10000)
+km100_info <- rbind(meta_metabo, km100$cluster)
+table(as.integer(km100_info[4,]))
+clusters_km100 <- lapply(seq_len(100), function(clst){
+  km100_info[c(3,1,2),which(km100_info[4,]==clst)]
+})
+km100_info[c(3,1,2),which(km100_info[4,]==(38))]
+
+write.xlsx(clusters_km100,
+           file = "~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/resulting_clusters_km100.xlsx")
+
+## visualise metabolite clusters in patients:
+mat_clust <- as.data.frame(t(data_metabo)) %>%
+  mutate(clustering = km100$cluster) %>%
+  group_by(clustering) %>%
+  summarize_all(mean) %>%
+  select(-clustering)
+
+matintr <- as.data.frame(t(mat_clust)[rownames(data_metabo),])
+
+mat2heatmap <- matintr %>%
+  rownames_to_column("patient") %>%
+  mutate(group = gr_info) %>%
+  arrange(group) %>%
+  column_to_rownames("patient")
+
+annot_row <- as.data.frame(mat2heatmap$group)
+rownames(annot_row) <- rownames(mat2heatmap)
+
+pheatmap::pheatmap(mat2heatmap[,-101], cluster_rows = F,
+                   annotation_row = annot_row)
+
+
+
+
+
+
+
+
+
 
 ### paired t-tests:
 sub_D <- sub_data[grep(pattern = "D", rownames(sub_data)),]
@@ -321,8 +409,150 @@ length(DE_2T)
 
 
 
+#######################################################
+##########    extract metabolites of inter   ##########
+##########           using RF models         ##########
+#######################################################
+
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/norm_data.RData")
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/meta_metabo.RData")
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/big_mat.RData")
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/rd_meta.RData")
+
+head(big_mat[,1:5])
+vars2rm <- colnames(rd_meta)[-c(4, 10)]
+grouped_mat <- big_mat %>%
+  group_by(COUPLENUMBER) %>%
+  select(-vars2rm)
+
+subst <- grouped_mat %>%
+  summarise_if(is.numeric, ~.[1]-.[2])# %>%
+  #rename_all(funs(paste0("subst_")))
+colnames(subst)[-1] <- paste0("subst_", colnames(subst))[-1]
+
+frac <- grouped_mat %>%
+  summarise_if(is.numeric, ~log(.[1]/.[2]))
+colnames(frac)[-1] <- paste0("frac_", colnames(frac))[-1]
+
+patient_info <- big_mat %>%
+  mutate(status = ifelse(test = grepl("D", METABONAME), yes = "D", no = "R"))
+
+donors <- patient_info %>%
+  filter(status == "D")
+colnames(donors)[-1] <- paste0("donor_", colnames(donors))[-1]
+
+recip <- patient_info %>%
+  filter(status == "R") %>%
+  select(-c(608:610, 612:663))
+colnames(recip)[-c(1,608)] <- paste0("recip_", colnames(recip))[-c(1, 608)]
+
+save(donors, file = "~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo_national/donors.RData")
+save(recip, file = "~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo_national/recip.RData")
+
+couple_mat <- plyr::join_all(list(subst, frac, donors[,-1], recip), by = "COUPLENUMBER")
+
+head(couple_mat[,1:5])
+rownames(couple_mat) <- couple_mat$COUPLENUMBER
+couple_mat <- couple_mat[,-1]
+new_mat <- couple_mat
+colnames(new_mat) <- make.names(colnames(couple_mat))
 
 
-### Normalise? :
-subpaths_norm <- apply(subpaths, 2, scale)
-rownames(subpaths_norm) <- rownames(subpaths)
+### RF
+rf_r<-randomForest::randomForest(Group~., new_mat, ntree=15000, mtry=100)
+rf_r
+plot(rf_r)
+tree_func(final_model = rf_r)
+
+### RF non tol vs tol
+gr <- as.character(new_mat$Group)
+gr[which((gr == "Primary_tolerant")|(gr == "Secondary_tolerant"))] <- "Tolerant"
+tol_non_tol_mat <- new_mat %>%
+  mutate(Group = as.factor(gr))
+rf_r<-randomForest::randomForest(Group~., tol_non_tol_mat, ntree=15000,
+                                 mtry=50, importance = T)
+rf_r
+plot(rf_r)
+tree_func(final_model = rf_r)
+rownames(rf_r$importance)[which(rf_r$importance==max(rf_r$importance))]
+
+topca <- tol_non_tol_mat %>% select_if(is.numeric)
+pca <- prcomp(topca)
+plot(pca$x,  col = tol_non_tol_mat$Group)
+sne <- Rtsne::Rtsne(topca, perplexity = 8)
+plot(sne$Y, col = tol_non_tol_mat$Group)
+
+
+
+#######################################################
+##########    metabo donors and recipients   ##########
+##########        compatibility_scores       ##########
+#######################################################
+
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/donors/subpaths_table_donors.RData")
+subpaths_donors <- subpaths
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/recip/subpaths_table.RData")
+subpaths_recip <- subpaths
+
+metabo_samples <- sample_subset <- read.xlsx("~/Documents/VIB/Projects/Integrative_Paris/documents_22:02:18/CYTOF_David_Michonneau/Data synthesis local cohort Saint-Louis 032018.xlsx",
+                                             check.names = FALSE) %>%
+  mutate(DATEOFCYTOFEXPERIMENT = as.Date(DATEOFCYTOFEXPERIMENT, "%d.%m.%Y"),
+         GROUP = tolower(GROUP)) %>%
+  dplyr::filter(!is.na(METABONAME)) %>%
+  dplyr::select_if(~sum(!is.na(.)) > 0)
+
+rd_couples <- metabo_samples$COUPLENUMBER[which(duplicated(metabo_samples$COUPLENUMBER)==TRUE)]
+samp_rd <- metabo_samples[which(metabo_samples$COUPLENUMBER %in% rd_couples),]
+rownames(samp_rd) <- samp_rd$METABONAME
+
+compatibility_scores <- compute_compatibility_score(samp_rd)
+
+## compute pca :
+sub_donors <- subpaths_donors[which(rownames(subpaths_donors)%in%rownames(samp_rd)),]
+sub_donors <- sub_donors[,which(colnames(sub_donors)%in%colnames(sub_recip))]
+sub_recip <- subpaths_recip[which(rownames(subpaths_recip)%in%rownames(samp_rd)),]
+sub_recip <- sub_recip[,which(colnames(sub_recip)%in%colnames(sub_donors))]
+
+mat2use <- rbind.data.frame(sub_donors, sub_recip, by = "col_names")
+mat2use <- mat2use[rownames(samp_rd),]
+mat2use_num <- apply(mat2use, 2, as.numeric)
+rownames(mat2use_num) <- rownames(mat2use)
+pca_metabo_rd <- prcomp(mat2use_num)
+
+
+pca_dist_scores <- compute_pca_distance_scores(pca_metabo_rd, samp_rd)
+
+couple_group <- lapply(unique(samp_rd$COUPLENUMBER), function(x){
+  couple <- samp_rd[which(samp_rd$COUPLENUMBER==x),]
+  group <- couple$GROUP[1]
+})
+couple_group <- unlist(couple_group)
+
+table1 <- cbind( compatibility_scores, pca_dist_scores)
+table1 <- table1[,-6]
+table1 <- as.data.frame(table1) %>%
+  mutate(group = couple_group)
+save(table1, file = "table1_metabo.RData")
+
+table1 %>% group_by(group) %>% summarise(mean(pc1_diff), sd(pc1_diff))
+table1 %>% group_by(group) %>% summarise(mean(pc2_diff), sd(pc2_diff))
+
+
+# random forest
+load("~/Documents/VIB/Projects/Integrative_Paris/Integrative/outputs/data/metabo/r&d/table1_metabo.RData")
+
+table1 <- table1[,-1]
+table1$group <- as.factor(table1$group)
+rf_r<-randomForest::randomForest(group~., table1, ntree=15000, mtry=3)
+rf_r
+plot(rf_r)
+tree_func(final_model = rf_r)
+
+red_table <- table1[which(table1$group!="non_tolerant"),]
+red_table$group <- as.factor(as.character(red_table$group))
+rf_r<-randomForest::randomForest(group~., red_table, ntree=15000, mtry=2)
+rf_r
+plot(rf_r)
+tree_func(final_model = rf_r)
+
+
