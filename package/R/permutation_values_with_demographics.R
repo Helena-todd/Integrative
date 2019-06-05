631081108365fcff2d612b6093543354693d5a51
Helena Todorov <helena_t@hotmail.fr>
2019-06-05 11:02:01 +0200

    New function to ci-ompute the permutation values while taking the demographics( age, gender..) into account

---
+++ package/R/permutation_values_with_demographics.R
@@ -0,0 +1,42 @@
+#' Permutations value with demographics
+#'
+#' This function takes as input a dataset and a feature id and returns the quantile value of the prediction score of the feature compared to the permutation distribution
+#'
+#' @param data A normalised dataset (dataframe with patients in rows and features in columns)
+#' @param nb_perm The number of permutations to generate the permutation distribution
+#' @param ind_feature the feature index, a column number
+#' @param orig_AUC the original AUC, computed only on the demographics
+#'
+#' @return the quantile value of the feature in the permutation distribution
+#' @export
+
+perm_val_demo <- function (data, nb_perm, ind_feature, orig_AUC){
+  print(paste0("Computing feature ", ind_feature))
+  quantInv <- function(distr, value) ecdf(distr)(value)
+  data_tmp <- data %>%
+    mutate(gender = as.factor(big_mat$GENDER),
+           cmv = as.factor(big_mat$CMVStatus))
+  res_perm <- list(AUC_demo = c(), AUC = c())
+  for(i in 1:(nb_perm-1)){
+    indices <- sample(1:nrow(data), size = nrow(data), replace = TRUE)
+    data_tmp[,"group"] <- data[indices, "group"]
+    fit1 <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])), data=data_tmp,trace=F)
+    fit2 <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])) + gender + cmv, data=data_tmp,trace=F)
+    res_tmp1 <- pROC::multiclass.roc(data_tmp$group,fitted(fit1))$auc[[1]]
+    res_tmp2 <- pROC::multiclass.roc(data_tmp$group,fitted(fit2))$auc[[1]]
+    diff_demo <- res_tmp2 - orig_AUC
+    res_perm[[1]] <- c(res_perm[[1]], res_tmp1)
+    res_perm[[2]] <- c(res_perm[[2]], diff_demo)
+  }
+  fit <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])), data=data,trace=F)
+  data_tmp[,"group"] <- data[, "group"]
+  fit_demo <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])) + gender + cmv, data=data_tmp,trace=F)
+  x <- pROC::multiclass.roc(data$group,fitted(fit))$auc[[1]]
+  x_demo <- pROC::multiclass.roc(data$group,fitted(fit_demo))$auc[[1]]
+  diff_demo <- x_demo - orig_AUC
+  qt <- quantInv(res_perm[[1]], x)
+  qt_demo <- quantInv(res_perm[[2]], diff_demo)
+  return(lst(quantile = qt, quantile_demo = qt_demo))
+}
+
+
