fcb0b146b4959e333ea0c6de3aff0e2458c16bbd
Helena Todorov <helena_t@hotmail.fr>
2019-06-11 17:43:41 +0200

    perm_val_demo_ I set replace to F when shuffling, otherwise the group variable sometimes contained only 2 classes instead of 3, which produced bugs.

--- package/R/permutation_values_with_demographics.R
+++ package/R/permutation_values_with_demographics.R
@@ -13,15 +13,18 @@
 perm_val_demo <- function (data, nb_perm, ind_feature, orig_AUC){
   print(paste0("Computing feature ", ind_feature))
   quantInv <- function(distr, value) ecdf(distr)(value)
-  data_tmp <- data %>%
-    mutate(gender = as.factor(big_mat$GENDER),
-           cmv = as.factor(big_mat$CMVStatus))
+  data_tmp <- data
   res_perm <- list(AUC_demo = c(), AUC = c())
   for(i in 1:(nb_perm-1)){
-    indices <- sample(1:nrow(data), size = nrow(data), replace = TRUE)
+    indices <- sample(1:nrow(data), size = nrow(data), replace = FALSE) # was TRUE before but sometimes generated bugs when it resultd in only 2 classes instaed of 3
     data_tmp[,"group"] <- data[indices, "group"]
-    fit1 <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])), data=data_tmp,trace=F)
-    fit2 <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])) + gender + cmv, data=data_tmp,trace=F)
+    # if(length(levels(as.factor(as.character(data_tmp[,"group"]))))!=3){
+    #   browser()
+    # }
+    #
+    # data_tmp$group <- as.factor(as.character(data_tmp$group))
+    fit1 <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])), data=data_tmp, trace=F)
+    fit2 <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])) + gender + age, data=data_tmp,trace=F)
     res_tmp1 <- pROC::multiclass.roc(data_tmp$group,fitted(fit1))$auc[[1]]
     res_tmp2 <- pROC::multiclass.roc(data_tmp$group,fitted(fit2))$auc[[1]]
     diff_demo <- res_tmp2 - orig_AUC
@@ -30,13 +33,14 @@ perm_val_demo <- function (data, nb_perm, ind_feature, orig_AUC){
   }
   fit <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])), data=data,trace=F)
   data_tmp[,"group"] <- data[, "group"]
-  fit_demo <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])) + gender + cmv, data=data_tmp,trace=F)
+  fit_demo <- nnet::multinom(group ~ eval(parse(text=colnames(data)[ind_feature])) + gender + age, data=data_tmp,trace=F)
   x <- pROC::multiclass.roc(data$group,fitted(fit))$auc[[1]]
   x_demo <- pROC::multiclass.roc(data$group,fitted(fit_demo))$auc[[1]]
   diff_demo <- x_demo - orig_AUC
   qt <- quantInv(res_perm[[1]], x)
   qt_demo <- quantInv(res_perm[[2]], diff_demo)
-  return(lst(quantile = qt, quantile_demo = qt_demo))
+
+  return(tibble::lst(quantile = qt, quantile_demo = qt_demo))
 }
 
 
