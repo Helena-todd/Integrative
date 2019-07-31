f4ba97ddeba2a74fe8daab461a773f3b9802a0fb
Helena Todorov <helena_t@hotmail.fr>
2019-07-31 11:57:29 +0200

    Small test function for the fcs_to_agg function

--- package/R/cyto_fcs_to_agg.R
+++ package/R/cyto_fcs_to_agg.R
@@ -1,4 +1,6 @@
-#' Title
+#' fcs_to_agg
+#'
+#' Aggregate fcs files into one tranform fcs file
 #'
 #' @param fcs_dir param
 #' @param fcs_names param
@@ -7,16 +9,21 @@
 #' @param output_name param
 #'
 #' @return aggregated flowframe as returned by the FlowSOM::AggregateFlowFrames function
+#' + the fcs file corresponding to it
 #' @export
 #'
 #' @examples bla
 fcs_to_agg <- function(fcs_dir, fcs_names, seed, cTotal, output_name){
-  setwd(dir = fcs_dir)
+  assert_that(
+    is.character(fcs_dir),
+    is.character(fcs_names),
+    is.character(output_name)
+  )
   set.seed(seed)
-  ff_agg <- FlowSOM::AggregateFlowFrames(fcs_names,
+  ff_agg <- FlowSOM::AggregateFlowFrames(paste0(fcs_dir, "/", fcs_names),
                                 cTotal= cTotal, writeOutput = TRUE,
-                                outputFile = output_name)
+                                outputFile = paste0(fcs_dir, "/", output_name))
   ff_agg <- flowCore::transform(ff_agg,
-                      flowCore::transformList(colnames(ff_agg@exprs)[c(3,17,28:62,71)], arcsinhTransform(b=1/5, a=0, c=0)))
+                      flowCore::transformList(colnames(ff_agg@exprs)[c(3,17,28:62,71)], flowCore::arcsinhTransform(b=1/5, a=0, c=0)))
   return(ff_agg)
 }
