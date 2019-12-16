07a7537603e135f2afc739b0d9241f84bc00e5f2
Helena Todorov <helena_t@hotmail.fr>
2019-12-16 12:47:53 +0100

    Function to gather the data from recipients, donors and couples into one

---
+++ package/R/integ_gather_DRDR.R
@@ -0,0 +1,78 @@
+#' Gather Donors, Recipients and D-R
+#'
+#' This function takes as imput: information on the patients under the form of
+#' a couple_info dataframe, as well as the path to the matrices of the selected
+#' features for recipients, donors and couples.
+#' optionally, it can also take the lists of genes that had the same behaviour
+#' in the two cohorts and select only these features.
+#' It returns a big dataframe, regrouping the selected features of recipients,
+#' donors and couples, as well as some meta info.
+#'
+#' @param data_couples The path to the matrix with couples in rows and selected feaatures in columns
+#' @param beh_couples The path to the list of couple features that had the same behaviour in the 2 cohorts
+#' @param data_recip The path to the matrix with recip in rows and selected feaatures in columns
+#' @param beh_recip The path to the list of recip features that had the same behaviour in the 2 cohorts
+#' @param data_donors The path to the matrix with donors in rows and selected feaatures in columns
+#' @param beh_donors The path to the list of donor features that had the same behaviour in the 2 cohorts
+#' @param couple_info The dataframe containing all donors and recipients in rows and meta information on them
+#' @param couple_info_2keep The columns in couple info that you wish to keep
+#'
+#' @return a dataframe containing the groupes features of D, R, D-R and some meta info
+#' @export
+
+gather_DRDR <- function(data_couples, beh_couples = NULL,
+                        data_recip, beh_recip = NULL,
+                        data_donors, beh_donors = NULL,
+                        couple_info, couple_info_2keep){
+  load(data_couples)
+  if(!is.null(beh_couples)){
+    # select only the features that had the same behaviour in the two cohorts
+    sel_beh <- read.xlsx(beh_couples)
+    pctgs_sel_ft_couples_90 <- pctgs_sel_ft_couples_90[,c("group", "COUPLENUMBER", sel_beh$sel_beh)]
+  }
+  # add "couple_" in front of the feature names
+  colnames2keep <- which(colnames(pctgs_sel_ft_couples_90) %in% c("group", "COUPLENUMBER"))
+  colnames(pctgs_sel_ft_couples_90)[-colnames2keep] <-
+    gsub("X", "couple_", colnames(pctgs_sel_ft_couples_90)[-colnames2keep])
+  # combine feature info with couple info about gender_comp...
+  pctgs_couples <- couple_info %>%
+    rownames_to_column("Id.Cryostem.R") %>%
+    dplyr::select(couple_info_2keep) %>%
+    dplyr::filter(grepl("R", rownames(couple_info))) %>%
+    inner_join(pctgs_sel_ft_couples_90, by = "COUPLENUMBER")
+
+  # recip info:
+  load(data_recip)
+  if(!is.null(beh_recip)){
+    # select only the features that had the same behaviour in the two cohorts
+    sel_beh <- read.xlsx(beh_recip)
+    pctgs_sel_ft_recip_90 <- pctgs_sel_ft_recip_90[,c("group", "Id.Cryostem.R", sel_beh$sel_beh)]
+  }
+  colnames2keep <- which(colnames(pctgs_sel_ft_recip_90) %in% c("group", "Id.Cryostem.R"))
+  colnames(pctgs_sel_ft_recip_90)[-colnames2keep] <-
+    gsub("X", "R_", colnames(pctgs_sel_ft_recip_90)[-colnames2keep])
+  pctgs_recip <- couple_info %>%
+    rownames_to_column("Id.Cryostem.R") %>%
+    dplyr::select("Id.Cryostem.R", "COUPLENUMBER") %>%
+    right_join(pctgs_sel_ft_recip_90, by = "Id.Cryostem.R")
+
+  # donor info :
+  load(data_donors)
+  if(!is.null(beh_donors)){
+    # select only the features that had the same behaviour in the two cohorts
+    sel_beh <- read.xlsx(beh_donors)
+    pctgs_sel_ft_donor_90 <- pctgs_sel_ft_donor_90[,c("group", "Id.Cryostem.R", sel_beh$sel_beh)]
+  }
+  colnames2keep <- which(colnames(pctgs_sel_ft_donor_90) %in% c("group", "Id.Cryostem.R"))
+  colnames(pctgs_sel_ft_donor_90)[-colnames2keep] <-
+    gsub("X", "D_", colnames(pctgs_sel_ft_donor_90)[-colnames2keep])
+  pctgs_donor <- couple_info %>%
+    rownames_to_column("Id.Cryostem.R") %>%
+    dplyr::select("Id.Cryostem.R", "COUPLENUMBER") %>%
+    right_join(pctgs_sel_ft_donor_90, by = "Id.Cryostem.R")
+
+  # combine :
+  pctgs_all <- pctgs_couples %>%
+    left_join(pctgs_recip, by = c("Id.Cryostem.R", "COUPLENUMBER", "group")) %>%
+    left_join(pctgs_donor, by = c("COUPLENUMBER", "group"))
+}
