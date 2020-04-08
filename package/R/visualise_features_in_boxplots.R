b75c116d72d19540e1e25a390457b2673c7d2bdc
Helena Todorov <helena_t@hotmail.fr>
2020-04-08 08:45:55 +0200

    small change for adapted color scale for TNT and PTST comparison

--- package/R/visualise_features_in_boxplots.R
+++ package/R/visualise_features_in_boxplots.R
@@ -5,28 +5,45 @@
 #' @return Boxplots
 #' @export
 
-visualise_features_in_boxplots <- function(list_common_features){
+visualise_features_in_boxplots <- function(list_common_features, compar = "TNT"){
   for(i in list_common_features$common_features){
     cryo_tmp <- common_features$local_df %>%
-      select(i, group) %>% magrittr::set_colnames(c("Var", "group"))
+      dplyr::select(i, group) %>% magrittr::set_colnames(c("Var", "group"))
 
     louis_tmp <- common_features$other_df %>%
-      select(i, group) %>% magrittr::set_colnames(c("Var", "group"))
+      dplyr::select(i, group) %>% magrittr::set_colnames(c("Var", "group"))
 
     ymin <- min(c(cryo_tmp$Var, louis_tmp$Var))
     ymax <- max(c(cryo_tmp$Var, louis_tmp$Var))
 
-    g1<- ggplot(cryo_tmp) + geom_boxplot(aes(x = group, y = Var, col = group)) +
-      geom_jitter(aes(x = group, y = Var, col = group)) + theme_minimal() +
-      coord_cartesian(ylim = c(ymin, ymax)) +
-      ggtitle(paste0(i," in Cryostem"))+
-      theme(plot.title = element_text(size = 10, face = "bold"))
+    if(compar == "TNT"){
+      g1<- ggplot(cryo_tmp) + geom_boxplot(aes(x = group, y = Var, col = group)) +
+        geom_jitter(aes(x = group, y = Var, col = group)) + theme_minimal() +
+        coord_cartesian(ylim = c(ymin, ymax)) +
+        ggtitle(paste0(i," in Cryostem"))+
+        theme(plot.title = element_text(size = 10, face = "bold"))
 
-    g2<- ggplot(louis_tmp) + geom_boxplot(aes(x = group, y = Var, col = group)) +
-      geom_jitter(aes(x = group, y = Var, col = group)) + theme_minimal() +
-      coord_cartesian(ylim = c(ymin, ymax)) +
-      ggtitle(paste0(i," in St Louis")) +
-      theme(plot.title = element_text(size = 10, face = "bold"))
+      g2<- ggplot(louis_tmp) + geom_boxplot(aes(x = group, y = Var, col = group)) +
+        geom_jitter(aes(x = group, y = Var, col = group)) + theme_minimal() +
+        coord_cartesian(ylim = c(ymin, ymax)) +
+        ggtitle(paste0(i," in St Louis")) +
+        theme(plot.title = element_text(size = 10, face = "bold"))
+    } else if(compar == "PTST"){
+      g1<- ggplot(cryo_tmp) + geom_boxplot(aes(x = group, y = Var, col = group)) +
+        geom_jitter(aes(x = group, y = Var, col = group)) + theme_minimal() +
+        scale_color_manual(values = c("green3", "blue")) +
+        coord_cartesian(ylim = c(ymin, ymax)) +
+        ggtitle(paste0(i," in Cryostem"))+
+        theme(plot.title = element_text(size = 10, face = "bold"))
+
+      g2<- ggplot(louis_tmp) + geom_boxplot(aes(x = group, y = Var, col = group)) +
+        geom_jitter(aes(x = group, y = Var, col = group)) + theme_minimal() +
+        scale_color_manual(values = c("green3", "blue")) +
+        coord_cartesian(ylim = c(ymin, ymax)) +
+        ggtitle(paste0(i," in St Louis")) +
+        theme(plot.title = element_text(size = 10, face = "bold"))
+    }
+
     gmix <- patchwork::wrap_plots(g1, g2)
     print(gmix)
   }
