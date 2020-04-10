4a02a658d02c4b316314fc183d041236e0a91ca0
Helena Todorov <helena_t@hotmail.fr>
2020-04-10 10:16:54 +0200

    Fixed an error in plot_correlations

--- package/R/plot_correlations.R
+++ package/R/plot_correlations.R
@@ -3,10 +3,13 @@
 #' @param ft_df The dataframe with samples in rows, features + group in columns
 #' @param compar The comparison: either "TNT" or "PS"
 #'
-#' @return A plot
+#' @return A plot and the graph
 #' @export
 #'
-plot_correlations <- function(ft_df, compar){
+plot_correlations <- function(ft_df,
+                              ft_file_other_cohort,
+                              compar,
+                              pval_threshold){
   correlations <- ft_df %>%
     dplyr::select(-group) %>%
     as.matrix %>%
@@ -16,9 +19,62 @@ plot_correlations <- function(ft_df, compar){
     gather(var2, value, -var1) %>%
     dplyr::filter(var1 < var2)
 
-  correlated_features <- correlations %>%
-    dplyr::filter(value > 0.7)
-  gr <- graph_from_data_frame(correlated_features, directed = FALSE,
+  # cor.tests to define which correlations are significant
+  cor_sel <- correlations[which(correlations[,3] > 0),]
+  pvals <- rep(1, nrow(cor_sel))
+  for(i in 1: nrow(cor_sel)){
+    var1 <- ft_df[,cor_sel[i,1]]
+    var2 <- ft_df[,cor_sel[i,2]]
+    pvals[i] <- cor.test(var1, var2, method = "spearman", exact = FALSE)$p.value
+  }
+
+  cor_sel <- cor_sel[which(pvals < pval_threshold),]
+
+  other <- readRDS(ft_file_other_cohort) %>%
+    dplyr::select(colnames(ft_df))
+  #
+  # load(ft_file_other_cohort)
+  # other <- rna_recip_cryo_TNT_90 %>%
+  #   dplyr::select(colnames(ft_df))
+
+  correlations_other <- other %>%
+    dplyr::select(-group) %>%
+    as.matrix %>%
+    cor %>%
+    as.data.frame %>%
+    rownames_to_column(var = 'var1') %>%
+    gather(var2, value, -var1) %>%
+    dplyr::filter(var1 < var2)
+
+  # cor.tests to define which correlations are significant
+  cor_sel_other <- correlations_other[which(correlations_other[,3] > 0),]
+  pvals_other <- rep(1, nrow(cor_sel_other))
+  for(i in 1: nrow(cor_sel_other)){
+    var1 <- other[,cor_sel_other[i,1]]
+    var2 <- other[,cor_sel_other[i,2]]
+    pvals_other[i] <- cor.test(var1, var2, method = "spearman", exact = FALSE)$p.value
+  }
+
+  cor_sel_other <- cor_sel_other[which(pvals_other < pval_threshold),]
+
+  # correlated_features <- correlations %>%
+  #   dplyr::filter(value > 0.7)
+  # correlated_other <- correlations_other %>%
+  #   dplyr::filter(value > 0.7)
+
+  # correlated_both <- intersect(correlated_features[,1:2], correlated_other[,1:2])
+  correlated_both <- dplyr::intersect(cor_sel[,1:2], cor_sel_other[,1:2])
+  cor <- rep(0, nrow(correlated_both))
+  for(i in 1:nrow(correlated_both)){
+    cor1 <- cor_sel[which((cor_sel[,1] == correlated_both[i,1]) &
+                                        (cor_sel[,2] == correlated_both[i,2])), 3]
+    cor2 <- cor_sel_other[which((cor_sel_other[,1] == correlated_both[i,1]) &
+                                        (cor_sel_other[,2] == correlated_both[i,2])), 3]
+    cor[i] <- mean(cor1, cor2)
+  }
+  correlated_both$value <- cor
+
+  gr <- graph_from_data_frame(correlated_both, directed = FALSE,
                               vertices=colnames(ft_df)[-which(colnames(ft_df)=="group")])
   nodes <- get.vertex.attribute(gr)
 
@@ -55,7 +111,8 @@ plot_correlations <- function(ft_df, compar){
   }
 
   set.seed(1)
-  plot(gr, vertex.size = 7, vertex.label.cex = .5, edge.width = (E(gr)$value - .7) * 20,
+  plot(gr, vertex.size = 5, vertex.label.cex = .6, edge.width = (E(gr)$value - round(min(E(gr)$value), digits = 2)) * 10,
        vertex.color = color_nodes, layout = layout_nicely)
+  return(gr)
 }
 
