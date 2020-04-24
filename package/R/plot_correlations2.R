c8f14dd1b59c048c8098dc9c13d6dfab5658e4a1
Helena Todorov <helena_t@hotmail.fr>
2020-04-24 09:51:53 +0200

    new plot cerrelation function to also plot D versus R

---
+++ package/R/plot_correlations2.R
@@ -0,0 +1,95 @@
+#' plot_correlations
+#'
+#' @param ft_df The dataframe with samples in rows, features + group in columns
+#' @param compar The comparison: either "TNT" or "PS"
+#'
+#' @return A plot and the graph
+#' @export
+#'
+plot_correlations2 <- function(ft_df,
+                               ft_file_other_cohort,
+                               compar = "TNT",
+                               node_color = NULL,
+                               pval_threshold){
+  correlations <- ft_df %>%
+    as.matrix %>%
+    cor %>%
+    as.data.frame %>%
+    rownames_to_column(var = 'var1') %>%
+    gather(var2, value, -var1) %>%
+    dplyr::filter(var1 < var2)
+
+  # cor.tests to define which correlations are significant
+  cor_sel <- correlations[which(correlations[,3] > 0),]
+  pvals <- rep(1, nrow(cor_sel))
+  for(i in 1: nrow(cor_sel)){
+    var1 <- ft_df[,cor_sel[i,1]]
+    var2 <- ft_df[,cor_sel[i,2]]
+    pvals[i] <- cor.test(var1, var2, method = "spearman", exact = FALSE)$p.value
+  }
+
+  cor_sel <- cor_sel[which(pvals < pval_threshold),]
+
+  if(grepl(".RDS", ft_file_other_cohort)){
+    other <- readRDS(ft_file_other_cohort) %>%
+      dplyr::select(colnames(ft_df))
+  } else if(grepl(".RData", ft_file_other_cohort)){
+    tmp_name <- load(ft_file_other_cohort)
+    other <- eval(parse(text = tmp_name)) %>%
+      dplyr::select(colnames(ft_df))
+  }
+
+  correlations_other <- other %>%
+    as.matrix %>%
+    cor %>%
+    as.data.frame %>%
+    rownames_to_column(var = 'var1') %>%
+    gather(var2, value, -var1) %>%
+    dplyr::filter(var1 < var2)
+
+  # cor.tests to define which correlations are significant
+  cor_sel_other <- correlations_other[which(correlations_other[,3] > 0),]
+  pvals_other <- rep(1, nrow(cor_sel_other))
+  for(i in 1: nrow(cor_sel_other)){
+    var1 <- other[,cor_sel_other[i,1]]
+    var2 <- other[,cor_sel_other[i,2]]
+    pvals_other[i] <- cor.test(var1, var2, method = "spearman", exact = FALSE)$p.value
+  }
+
+  cor_sel_other <- cor_sel_other[which(pvals_other < pval_threshold),]
+
+  # correlated_features <- correlations %>%
+  #   dplyr::filter(value > 0.7)
+  # correlated_other <- correlations_other %>%
+  #   dplyr::filter(value > 0.7)
+
+  # correlated_both <- intersect(correlated_features[,1:2], correlated_other[,1:2])
+  correlated_both <- dplyr::intersect(cor_sel[,1:2], cor_sel_other[,1:2])
+  cor <- rep(0, nrow(correlated_both))
+  for(i in 1:nrow(correlated_both)){
+    cor1 <- cor_sel[which((cor_sel[,1] == correlated_both[i,1]) &
+                            (cor_sel[,2] == correlated_both[i,2])), 3]
+    cor2 <- cor_sel_other[which((cor_sel_other[,1] == correlated_both[i,1]) &
+                                  (cor_sel_other[,2] == correlated_both[i,2])), 3]
+    cor[i] <- mean(cor1, cor2)
+  }
+  correlated_both$value <- cor
+
+  gr <- graph_from_data_frame(correlated_both, directed = FALSE,
+                              vertices=colnames(ft_df))
+  nodes <- get.vertex.attribute(gr)
+  color_nodes <- node_color
+
+
+  set.seed(1)
+  plot(gr, vertex.size = 5, vertex.label.cex = .6, edge.width = (E(gr)$value - round(min(E(gr)$value), digits = 2)) * 10,
+       vertex.color = color_nodes, layout = layout_nicely)
+  legend(1, 1, legend=c(round(min(E(gr)$value), digits = 3), round(max(E(gr)$value), digits = 3)),
+         col=c("grey", "grey"),
+         lwd=c((min(E(gr)$value) - round(min(E(gr)$value), digits = 2)) * 10,
+               (max(E(gr)$value) - round(min(E(gr)$value), digits = 2)) * 10),
+         cex=0.8,
+         title="Edge weight", text.font=4, bg='white')
+  return(gr)
+}
+
